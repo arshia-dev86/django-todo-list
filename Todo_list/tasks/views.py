ae2d92f33148e348d4d1ce71e50ee48ce852c0c8
Arshia Eisazadeh <arshia.1386ae@gmail.com>
2026-08-15 14:59:37 +0330

    feat: add task completion and empty state

--- Todo_list/tasks/views.py
+++ Todo_list/tasks/views.py
@@ -31,17 +31,18 @@ def add_task(request):
 
 def delete_task(request, task_id):
 
-    task= get_object_or_404(Task , id=task_id)
-
     if request.method == 'POST':
+        task= get_object_or_404(Task , id=task_id)
         task.delete()
         return redirect('tasks') 
            
     return render(request, 'tasks/delete_task.html', {'task': task})
 
 def edit_task(request, task_id):
-    task= get_object_or_404(Task, id=task_id)
     if request.method == 'POST':
+
+        task= get_object_or_404(Task, id=task_id)
+
 
         task.title= request.POST.get('title')
         task.description= request.POST.get('description')
@@ -54,3 +55,13 @@ def edit_task(request, task_id):
 
         return redirect("tasks")
     return render(request, 'tasks/edit_task.html', {'task': task})
+
+
+
+
+def complete_task(request, task_id):
+    if request.method == 'POST' :
+        task = get_object_or_404(Task, id=task_id)
+        task.done = True
+        task.save()
+    return redirect('tasks')
